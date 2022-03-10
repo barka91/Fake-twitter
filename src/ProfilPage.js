ecdd31c44397f66a9147315b936d10047210edd0
Barka Abkress <barka.abkress@gmail.com>
2022-03-10 14:45:08 +0100

    ajout ProfilPage

---
+++ src/ProfilPage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import "./ProfilPage.css"
+import avatar from "./media/1ceac0b83e8307449c91f21113b21762.jpg"
+import banniere from "./media/Vogue_Merry_Anime_Infobox.png"
+
+class ProfilPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='profil'>
+                <div className='avatar'>
+                    <img src={avatar}></img>
+                </div>
+                <div className='banniere'>
+                    <img src={banniere}/>
+                </div>
+                <div className='username'>
+                    <span>usopp </span>
+                </div>
+                <div className='id'>
+                    <span>@sogeking</span>
+                </div>
+                <div className='Description'>
+                    <span>Le vrai capitaine</span>
+                </div>                
+                <div className='info'>
+                    <span>Sirop, East Blue</span>
+                    <span>Naissance le 1 avril 2001</span>
+                    <span></span>
+                </div>
+
+
+            </div>
+        )
+
+    }
+}
+export default ProfilPage;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -8,8 +8,9 @@ import NavigationPannel from "./NavigationPannel"
 import Logout from "./Logout"
 import Login from "./Login"
 import TweetBox from "./TweetBox"
+import ProfilPage from './ProfilPage';
 
-ReactDOM.render(<MainPage page="connexion" />, document.getElementById('root'));
+ReactDOM.render(<ProfilPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
